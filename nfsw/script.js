336afb05a0548f0749644c598d44620e201d1ed9
BifTech <tr.eren610@gmail.com>
2026-02-08 20:30:36 +0300

    Update script.js

--- nfsw/script.js
+++ nfsw/script.js
@@ -6,10 +6,8 @@ const playerTitle = document.getElementById("playerTitle");
 function openPlayer(title, src){
   home.style.display = "none";
   playerPage.style.display = "block";
-
   playerTitle.innerText = title;
   playerVideo.src = src;
-
   window.scrollTo(0,0);
 }
 
@@ -18,4 +16,16 @@ function goHome(){
   playerVideo.src = "";
   playerPage.style.display = "none";
   home.style.display = "block";
+}
+
+/* TAB SWITCH */
+function showTab(type, el){
+  document.getElementById("videoTab").style.display =
+    type === "video" ? "block" : "none";
+
+  document.getElementById("photoTab").style.display =
+    type === "photo" ? "block" : "none";
+
+  document.querySelectorAll(".tab").forEach(t=>t.classList.remove("active"));
+  el.classList.add("active");
 }
